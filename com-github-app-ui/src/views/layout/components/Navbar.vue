0a3d5d60fbc3ae3cadfead182954cd6a61d6c64d
xushuyang <xushuyang@foton.com.cn>
2018-05-21 14:54:49 +0800

    123

--- com-github-app-ui/src/views/layout/components/Navbar.vue
+++ com-github-app-ui/src/views/layout/components/Navbar.vue
@@ -5,25 +5,32 @@
         <img :src="img_logo" style="float:left;display:block;width:125px;height:40px;margin-top:12px;"/><span class="title-font">{{$t("message.title")}}</span>
       </div>
     </el-col>
-    <el-col :xs="12" :sm="14" :md="15" :lg="16" :xl="19">
+    <el-col :xs="12" :sm="14" :md="15" :lg="16" :xl="19" >
       <el-row type="flex" justify="end" align="middle" ><el-col :span="24">
-        <el-dropdown :hide-on-click="false" style="float:right;">
-          <span><svg-icon icon-class="mine" font-size="20px"/></span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>黄金糕</el-dropdown-item>
-            <el-dropdown-item>狮子头</el-dropdown-item>
-            <el-dropdown-item>螺蛳粉</el-dropdown-item>
-            <el-dropdown-item disabled>双皮奶</el-dropdown-item>
-            <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
+        <div style="margin-right:24px;margin-left:24px;height:64px;line-height:64px;float:right;text-align:center;">
+          <el-dropdown :hide-on-click="false" style="float:right;">
+            <span><svg-icon icon-class="mine" font-size="20px"/></span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>黄金糕</el-dropdown-item>
+              <el-dropdown-item>狮子头</el-dropdown-item>
+              <el-dropdown-item>螺蛳粉</el-dropdown-item>
+              <el-dropdown-item divided>双皮奶</el-dropdown-item>
+              <el-dropdown-item>蚵仔煎</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
         <el-menu mode="horizontal" text-color="#FFF" background-color="#64a51a" active-text-color="#d1dbe5" class="nav-menu" @select="menuClick">
           <el-menu-item index="1">首页</el-menu-item>
           <el-menu-item index="2">基础配置</el-menu-item>
           <el-menu-item index="3">数据接口</el-menu-item>
           <el-menu-item index="4">平台监控</el-menu-item>
           <el-menu-item index="5">统计分析</el-menu-item>
-          <el-menu-item index="6">终端运营</el-menu-item>
+          <el-submenu index="6">
+            <template slot="title">终端运营</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-submenu>
         </el-menu>
       </el-col></el-row>
     </el-col>
@@ -72,10 +79,12 @@ export default {
   background-color: #64a51a;
 }
 .nav-menu {
-  height: 64px;
   font-weight:bold;
   font-family: "Hiragino Sans GB";
   float: right;
+  margin-top:1px!important;
+  height:64px;
+  line-height:64px;
 }
 </style>
 
